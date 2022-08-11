a1cd4e4b3f89f32a452f3979eec442ff149c4e7e
DavidRBelanger <94973702+DavidRBelanger@users.noreply.github.com>
2022-08-11 13:04:15 -0400

    Basic Methods for TransactionClass created
    Basic methods finalized. More may be needed in future.

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,12 +1,57 @@
+
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author drbx3
  */
 public class Transaction {
+
+    private ArrayList itemList;
+    private Double totalPrice;
+    private int itemCount;
+    private double tax;
+
+    public Transaction() {
+        itemList = new ArrayList<Item>();
+        totalPrice = 0.0;
+        itemCount = 0;
+        tax = 0;
+    }
+
+    public Transaction(double tax) {
+        itemList = new ArrayList<Item>();
+        totalPrice = 0.0;
+        itemCount = 0;
+        this.tax = tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public ArrayList getList() {
+        return itemList;
+    }
+
+    public void addItem(Item a) {
+        itemList.add(a);
+        totalPrice += a.getTaxedPrice(tax);
+        itemCount++;
+    }
+
+    public void voidItem(int index) {
+        itemList.remove(index);
+    }
+
+    public void addTransactionDiscount(double disc) { //disc should be a number between 0 and 1. if you want a 10% discount, .1 would be entered.
+        totalPrice = totalPrice * (1 - disc);
+    }
     
+    
+
 }
