5fed1b20f6c72f5fa22d3aec446ec9020bc4192d
DavidRBelanger <94973702+DavidRBelanger@users.noreply.github.com>
2022-08-11 15:09:57 -0400

    Day Class and changes to register and item
    Started the day class. The register class was changed to a JPanel swing GUI. I changed item so that tax was managed on each item, instead of on a transaction basis.

--- src/Day.java
+++ src/Day.java
@@ -1,12 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author drbx3
  */
 public class Day {
+
+    private ArrayList transactionList;
+    private Date date;
+
+    public Day() {
+        transactionList = new ArrayList<Transaction>();
+        date = new Date();
+    }
+
+    public void addTransaction(Transaction a) {
+        transactionList.add(a);
+    }
+
+    public void removeTransaction(int index) {
+        transactionList.remove(index);
+    } 
+    
+    
     
 }
--- src/Item.java
+++ src/Item.java
@@ -3,32 +3,51 @@
  */
 /**
  *
- * 
+ *
  */
 public class Item {
+
     private int departmentNum; //two digit number. department numbers are included in DEPTLIST.txt which is initialized when creating store
     private String itemName; //name of product
     private int styleNum; //6 digit number for lookup purposes
     private double price; //price
+    private boolean isTax;
     public Item(String nm) {
         itemName = nm;
     }
+
     public void setName(String nm) {
         itemName = nm;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
+
     public void setStyle(int style) {
         styleNum = style;
     }
+
     public String getName() {
         return itemName;
     }
+
     public int getDepartment() {
         return departmentNum;
     }
+
+    public int getStyle() {
+        return styleNum;
+    }
+
+    public double getPrice() {
+        return price;
+    }
     
-        
-        
+    public double getTaxedPrice(double tax) { //tax should be a number between 0 and 1. 7% tax is .07.
+       if (isTax)
+           return price*(1+tax);
+       else
+           return price;
+    }
 }
