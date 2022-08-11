4447c45a4ecbb0863c54cce205180f74199fcfae
DavidRBelanger <94973702+DavidRBelanger@users.noreply.github.com>
2022-08-11 12:38:05 -0400

    Item Class Finalized
    Basic Methods, constructors, and variables created for the Item class. More may need to be added.

--- src/Item.java
+++ src/Item.java
@@ -6,8 +6,29 @@
  * 
  */
 public class Item {
-    private int departmentNum;
-    private String itemName;
-    private int styleNum;
+    private int departmentNum; //two digit number. department numbers are included in DEPTLIST.txt which is initialized when creating store
+    private String itemName; //name of product
+    private int styleNum; //6 digit number for lookup purposes
+    private double price; //price
+    public Item(String nm) {
+        itemName = nm;
+    }
+    public void setName(String nm) {
+        itemName = nm;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setStyle(int style) {
+        styleNum = style;
+    }
+    public String getName() {
+        return itemName;
+    }
+    public int getDepartment() {
+        return departmentNum;
+    }
     
+        
+        
 }
